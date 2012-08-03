ee079db2531ebd8f32d6fa610b5dc044e02ce908
Michael Bianco <iloveitaly@gmail.com>
2012-08-03 09:16:16 -0400

    Fixed issue where bundle items would not delete. Fixes #2

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -4,6 +4,7 @@ Spree::OrdersController.class_eval do
 	def bundle_populate
     @order = current_order(true)
 
+    # TODO since we don't need the index anymore this could be cleaned up
     existing_variants = {}
     @order.line_items.each_with_index { |l, i| existing_variants[l.variant.id] = i }
 
@@ -12,24 +13,23 @@ Spree::OrdersController.class_eval do
       variant_id = variant_id.to_i
 
       # update (not add) if already in cart
-      if not existing_variants[variant_id].nil?
-        if quantity > 0
-          @order.line_items[existing_variants[variant_id]].quantity = quantity 
-        else
-          @order.line_items.delete_at(variant_id)
-        end
-      else
+      if existing_variants[variant_id].nil?
         @order.add_variant(Spree::Variant.find(variant_id), quantity) if quantity > 0
+      else
+        @order.line_items[existing_variants[variant_id]].quantity = quantity 
       end
     end if params[:variants]
 
+    @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
+
     flash[:notice] = t(:added_to_cart)
 
-    @order.save
+    @order.save!
 
     fire_event('spree.cart.add')
     fire_event('spree.order.contents_changed')
 
+    # TODO this is a bit messy, must be a way to clean it up. URL parsing?
     if params.has_key? :checkout or request.env["HTTP_REFERER"].blank?
       redirect_to cart_path
     else
