5057eaea461abbdc600852e31e67eed120b472be
Michael Bianco <iloveitaly@gmail.com>
2013-04-01 17:04:18 -0400

    Tabs to spaces

--- app/controllers/spree/taxons_controller_decorator.rb
+++ app/controllers/spree/taxons_controller_decorator.rb
@@ -4,12 +4,12 @@ Spree::TaxonsController.class_eval do
   old_show = instance_method(:show)
 
   define_method(:show) do
-      @taxon = Spree::Taxon.find_by_permalink!(params[:id])
+    @taxon = Spree::Taxon.find_by_permalink!(params[:id])
 
-      if @taxon.taxon_splash.nil?
-        old_show.bind(self).()
-      else
-        render :template => 'spree/taxons/splash'
-      end
+    if @taxon.taxon_splash.nil?
+      old_show.bind(self).()
+    else
+      render :template => 'spree/taxons/splash'
+    end
   end
 end
--- app/models/spree/taxon_splash.rb
+++ app/models/spree/taxon_splash.rb
@@ -1,24 +1,24 @@
 module Spree
-	class TaxonSplash < ActiveRecord::Base
-		belongs_to :taxon, :autosave => true
-		validates_presence_of :taxon
-		attr_accessible :taxon_id, :content
-		
-		def is_leaf?
-			children.count == 0
-		end
+  class TaxonSplash < ActiveRecord::Base
+    belongs_to :taxon, :autosave => true
+    validates_presence_of :taxon
+    attr_accessible :taxon_id, :content
+    
+    def is_leaf?
+      children.count == 0
+    end
 
-		def children
-			begin
-				taxon.children.select { |c| not c.taxon_splash.nil? }.map { |c| c.taxon_splash }
-			rescue
-				[]
-			end
-		end 
+    def children
+      begin
+        taxon.children.select { |c| not c.taxon_splash.nil? }.map { |c| c.taxon_splash }
+      rescue
+        []
+      end
+    end 
 
-		def products
-			# TODO: see if products are manually defined, if not fallback to taxon products
-			taxon.products
-		end
-	end
+    def products
+      # TODO: see if products are manually defined, if not fallback to taxon products
+      taxon.products
+    end
+  end
 end
